976ef622957cb7ae70c3a740166bb8b5ca12ae33
evankim1002-boop <evankim1002@gmail.com>
2026-06-06 15:21:51 -0700

    changed

--- Book.java
+++ Book.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+
 public class Book {
+    //private variable
+    private ArrayList<Item> book;
+    private int itemCount;
+    
+
+    //constructor
+    public Book(){
+        this.book = new ArrayList<Item>();
+        this.itemCount = 0;
+    }
+
+    //method
+
+    public void addItem(Item item){
+        book.add(item);
+        itemCount++;
+    }
+
+    public Item getItem(int i){
+        return book.get(i);
+    }
+
+    public int getItemCount(){
+        return itemCount;
+    }
+
+    public void deleteItem(int i){
+        book.remove(i);
+        itemCount--;
+    }
+
+    public void clearBook(){
+        book.clear();
+        itemCount = 0;
+    }
+
     
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
-public Main {
-    Scanner scanner = new Scanner();
-    private String userInput;
+public class Main {
+    
+    
 
+
+    public static void main(String[] args) {
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the crafting calculator!");
+        System.out.println("What item would you like to craft?");
+        userInput = scanner.nextLine();
+        Item item = new Item(userInput);
+        System.out.println("How many " + userInput + " would you like to craft?");
+        int count = scanner.nextInt();
+        item.addNeededCount(count);
+        System.out.println("You will need " + item.getNeededCount() + " " + userInput + " to craft " + count + " " + userInput);
+    }
 }
